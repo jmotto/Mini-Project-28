a210f78c1f5e10b3ffe0e33290496e41d86f3c05
Jamie Otto <jmotto31@gmail.com>
2022-10-03 21:20:51 -0700

    initial

---
+++ models/Student.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const studentSchema = new Schema(
+  {
+    first: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    last: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+studentSchema
+  .virtual('getAssignments')
+  // Getter
+  .get(function () {
+    return this.assignments.length;
+  });
+
+
+const Student = model('student', studentSchema);
+
+module.exports = Student;
